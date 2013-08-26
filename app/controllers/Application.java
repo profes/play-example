cb6a120a11a06a44e84edfdb8f81265b561ded40
damian <damian@atchik.com>
2013-08-27 00:31:44 +0200

    playing with async response just to see how it works

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,12 +2,15 @@ package controllers;
 
 import infrastructure.Sender;
 import org.codehaus.jackson.JsonNode;
+import play.libs.Akka;
+import play.libs.F;
 import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.concurrent.Callable;
 
 import static play.Logger.debug;
 
@@ -23,15 +26,27 @@ public class Application extends Controller {
 
     @BodyParser.Of(BodyParser.Json.class)
     public Result send() {
-        JsonNode json = request().body().asJson();
+        final JsonNode json = request().body().asJson();
 
         debug("Got JSON: " + json);
 
         if (json == null) {
             return badRequest("Expecting Json data");
-        } else {
-            sender.send(json.toString());
-            return ok("Hello " + json);
         }
+        F.Promise<Boolean> promise = Akka.future(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                sender.send(json.toString());
+                return true;
+            }
+        });
+        return async(
+                promise.map(new F.Function<Boolean, Result>() {
+                    @Override
+                    public Result apply(Boolean o) throws Throwable {
+                        return ok("Hello " + json);
+                    }
+                })
+        );
     }
 }
